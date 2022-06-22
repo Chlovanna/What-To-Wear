8ff98d210eadc7f4cf03614d0be7ae81b86db86e
Chlovanna <ashdmt39@gmail.com>
2022-06-22 19:49:25 -0400

    start work on search-button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,187 +1,36 @@
-//Variable to store searched cities
-var city = "";
-// variable declaration
-var searchCity = $("#search-city");
-var searchButton = $("#search-button");
-var clearButton = $("#clear-history");
-var currentCity = $("#current-city");
-var currentTemperature = $("#temperature");
-var currentHumidty = $("#humidity");
-var currentWSpeed = $("#wind-speed");
-var currentUvindex = $("#uv-index");
-var sCity = [];
-// To validate city existance in storage
-function find(c) {
-  for (var i = 0; i < sCity.length; i++) {
-    if (c.toUpperCase() === sCity[i]) {
-      return -1;
-    }
-  }
-  return 1;
+const handleClick = ()=>{
+  let city = document.querySelector('input').value;
+  if(!city) return;
+
+  let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit1&appid=${APIKey}`;
+
+fetch(url).then(data=>data.json()).then(({lat,lon})=>{
+  let url2 = 
+  fetch
+});
 }
-// Third-party server side API key
-var APIKey = "870df081fe67aa9168f9a9391dfa26cd";
-//To display the five day forecast in the current city
-function displayWeather(event) {
-  event.preventDefault();
-  if (searchCity.val().trim() !== "") {
-    city = searchCity.val().trim();
-    currentWeather(city);
-  }
-}
-// AJAX call
-function currentWeather(city) {
-  // the URL to get a data from server side.
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&APPID=" +
-    APIKey;
+document.querySelector('#search-button').addEventListener('click', handleClick);
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    //to parse the response to display the current weather including the city name, date, and the weather icon.
-    console.log(response);
 
-    var weathericon = response.weather[0].icon;
-    var iconurl =
-      "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
 
-    var date = new Date(response.dt * 1000).toLocaleDateString();
-    //to parse the response for name of city and concanatig the date and icon.
-    $(currentCity).html(
-      response.name + "(" + date + ")" + "<img src=" + iconurl + ">"
-    );
 
-    var tempF = (response.main.temp - 273.15) * 1.8 + 32;
-    $(currentTemperature).html(tempF.toFixed(2) + "&#8457");
-    //humidity
-    $(currentHumidty).html(response.main.humidity + "%");
-    //Wind speed in MPH
-    var ws = response.wind.speed;
-    var windsmph = (ws * 2.237).toFixed(1);
-    $(currentWSpeed).html(windsmph + "MPH");
-    //to display UV Index
 
-    UVIndex(response.coord.lon, response.coord.lat);
-    forecast(response.id);
-    if (response.cod == 200) {
-      sCity = JSON.parse(localStorage.getItem("cityname"));
-      console.log(sCity);
-      if (sCity == null) {
-        sCity = [];
-        sCity.push(city.toUpperCase());
-        localStorage.setItem("cityname", JSON.stringify(sCity));
-        addToList(city);
-      } else {
-        if (find(city) > 0) {
-          sCity.push(city.toUpperCase());
-          localStorage.setItem("cityname", JSON.stringify(sCity));
-          addToList(city);
-        }
-      }
-    }
-  });
-}
-//UVIndex function
-function UVIndex(ln, lt) {
-  var uvqURL =
-    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
-    APIKey +
-    "&lat=" +
-    lt +
-    "&lon=" +
-    ln;
+// Current weather data
+//  "https://api.openweathermap.org/data/2.5/weather?q=" +
+//     city +
+//     "&APPID=" +
+//     APIKey;
 
-  $.ajax({
-    url: uvqURL,
-    method: "GET",
-  }).then(function (response) {
-    $(currentUvindex).html(response.value);
-  });
-}
+//   UV Index
+//    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+//     APIKey +
+//     "&lat=" +
+//     lt +
+//     "&lon=" +
+//     ln;  
 
-//the five-day forecast for the current city.
-function forecast(cityid) {
-  var dayover = false;
-
-  var queryforcastURL =
-    "https://api.openweathermap.org/data/2.5/forecast?id=" +
-    cityid +
-    "&appid=" +
-    APIKey;
-  $.ajax({
-    url: queryforcastURL,
-    method: "GET",
-  }).then(function (response) {
-    for (i = 0; i < 5; i++) {
-      var date = new Date(
-        response.list[(i + 1) * 8 - 1].dt * 1000
-      ).toLocaleDateString();
-      var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
-      var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
-      var tempK = response.list[(i + 1) * 8 - 1].main.temp;
-      var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(2);
-      var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
-
-      $("#fDate" + i).html(date);
-      $("#fImg" + i).html("<img src=" + iconurl + ">");
-      $("#fTemp" + i).html(tempF + "&#8457");
-      $("#fHumidity" + i).html(humidity + "%");
-    }
-  });
-}
-
-function addToList(c) {
-  var listEl = $("<li>" + c.toUpperCase() + "</li>");
-  $(listEl).attr("class", "list-group-item");
-  $(listEl).attr("data-value", c.toUpperCase());
-  $(".list-group").append(listEl);
-}
-// to invoke the past search again when the list group item is clicked in the search history
-function invokePastSearch(event) {
-  var liEl = event.target;
-  if (event.target.matches("li")) {
-    city = liEl.textContent.trim();
-    currentWeather(city);
-  }
-}
-
-function loadlastCity() {
-  $("ul").empty();
-  var sCity = JSON.parse(localStorage.getItem("cityname"));
-  if (sCity !== null) {
-    sCity = JSON.parse(localStorage.getItem("cityname"));
-    for (i = 0; i < sCity.length; i++) {
-      addToList(sCity[i]);
-    }
-    city = sCity[i - 1];
-    currentWeather(city);
-  }
-}
-//to clear the search history
-function clearHistory(event) {
-  event.preventDefault();
-  sCity = [];
-  localStorage.removeItem("cityname");
-  document.location.reload();
-}
-//the click handlers
-$("#search-button").on("click", displayWeather);
-$(document).on("click", invokePastSearch);
-$(window).on("load", loadlastCity);
-$("#clear-history").on("click", clearHistory);
-
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+//     5 day forecast
+//     "https://api.openweathermap.org/data/2.5/forecast?id=" +
+//     cityid +
+//     "&appid=" +
+//     APIKey;
